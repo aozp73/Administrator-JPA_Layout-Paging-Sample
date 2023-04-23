8445ee7a70ebb061d46a64c3c3f3b8972de08578
aozp73 <aozp73@naver.com>
2023-04-23 15:53:54 +0900

    initial commit

---
+++ src/main/java/shop/mtcoding/administrator_layout/AdministratorLayoutApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.administrator_layout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministratorLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministratorLayoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/administrator_layout/AdministratorLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.administrator_layout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministratorLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
